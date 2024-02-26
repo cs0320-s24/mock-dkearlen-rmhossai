6af5ff28e0ea67087c880fe8d6a38ee470686ae9
Devon Kear-Leng <devonkear11@gmail.com>
2024-02-26 16:52:00 -0500

    feat: verbose and basic change

--- mock/src/components/REPLHistory.tsx
+++ mock/src/components/REPLHistory.tsx
@@ -6,6 +6,7 @@ interface REPLHistoryProps{
 export function REPLHistory(props : REPLHistoryProps) {
     return (
         <div className="repl-history">
+            {/* This command puts the input onto the screen. */}
             {props.history.map((command, index) => <p> {command} </p>)}
             {/* TODO: To go through all the pushed commands... try the .map() function! */}
         </div>
--- mock/src/components/REPLInput.tsx
+++ mock/src/components/REPLInput.tsx
@@ -13,12 +13,28 @@ export function REPLInput(props : REPLInputProps) {
     // Remember: let React manage state in your webapp. 
     // Manages the contents of the input box
     const [commandString, setCommandString] = useState<string>('');
-    // TODO WITH TA : add a count state
+    // Creates a count state
     const [submitCount, addSubmitCount] = useState<number>(0);
+    // Creates a mode label
+    const[giveMode, changeMode] = useState<string>('brief');
+    // Creates a function to handle the submit button
     function handleSubmit(commandString: string){
       addSubmitCount(submitCount + 1);
       props.setHistory([...props.history, commandString]);
+      if (new String(commandString.toLowerCase()).valueOf === new String('mode').valueOf){
+        switchModes();
+       }
     }
+    // Creates a function to handle changing between modes
+    function switchModes(){
+      if (giveMode.localeCompare('brief') === 0) {
+        changeMode("verbose");
+      }
+      if (giveMode.localeCompare("verbose") === 0) {
+        changeMode("brief");
+      }
+      props.setHistory([...props.history, "Mode: " + giveMode]);  
+      }
     // TODO WITH TA: build a handleSubmit function called in button onClick
     // TODO: Once it increments, try to make it push commands... Note that you can use the `...` spread syntax to copy what was there before
     // add to it with new commands.
@@ -27,18 +43,24 @@ export function REPLInput(props : REPLInputProps) {
      * of the REPL and how they connect to each other...
      */
     return (
-        <div className="repl-input">
-            {/* This is a comment within the JSX. Notice that it's a TypeScript comment wrapped in
+      <div className="repl-input">
+        {/* This is a comment within the JSX. Notice that it's a TypeScript comment wrapped in
             braces, so that React knows it should be interpreted as TypeScript */}
-            {/* I opted to use this HTML tag; you don't need to. It structures multiple input fields
+        {/* I opted to use this HTML tag; you don't need to. It structures multiple input fields
             into a single unit, which makes it easier for screenreaders to navigate. */}
-            <fieldset>
-              <legend>Enter a command:</legend>
-              <ControlledInput value={commandString} setValue={setCommandString} ariaLabel={"Command input"}/>
-            </fieldset>
-            {/* TODO WITH TA: Build a handleSubmit function that increments count and displays the text in the button */}
-            {/* TODO: Currently this button just counts up, can we make it push the contents of the input box to the history?*/}
-            <button onClick={() => handleSubmit(commandString)}>Submitted {submitCount} times.</button>
-        </div>
+        <fieldset>
+          <legend>Enter a command:</legend>
+          <ControlledInput
+            value={commandString}
+            setValue={setCommandString}
+            ariaLabel={"Command input"}
+          />
+        </fieldset>
+        {/* TODO WITH TA: Build a handleSubmit function that increments count and displays the text in the button */}
+        {/* TODO: Currently this button just counts up, can we make it push the contents of the input box to the history?*/}
+        <button onClick={() => handleSubmit(commandString)}>
+          Submitted {submitCount} times.
+        </button>
+      </div>
     );
   }
